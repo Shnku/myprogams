bcd79bd0b9944c2819a7abf745cf50ad49a6da37
ShAnku <101804633+Shnku@users.noreply.github.com>
2022-12-25 23:23:57 +0530

    this added

---
+++ collage/cpp/oops/thispointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class employee
+{
+private:
+    int id;
+    float sal;
+    char nm[50];
+public:
+    employee() {}
+    ~employee() {}
+    
+};
+int main(){
+    
+
+return 0;
+}
