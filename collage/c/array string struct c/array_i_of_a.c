90ea11558cdcaecacd96aedf09b5100988c389e2
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-01-19 21:31:26 +0530

    successful

---
+++ collage/c/array string struct c/array_i_of_a.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+    int a[5], i;
+    printf("\ni am in b.c\n");
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", i[a]);
+    }
+    return 0;
+}
