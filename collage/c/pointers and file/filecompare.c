2baeda343b457a78c72ee8d66403d3a0c3bc8167
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-02-03 11:16:25 +0530

    file added

--- collage/c/pointers and file/filecompare.c
+++ collage/c/pointers and file/filecompare.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     FILE *fp1, *fp2;
     int flag = 0;
-    if (fp1=fopen(file1.txt, "r") || fp2=fopen(file2.txt, "r") == NULL)
+    if (((fp1 = fopen("file1.txt", "r")) == NULL) || ((fp2 = fopen("file2.txt", "r")) == NULL))
     {
         printf("file doesnoe exists;");
+        exit(1);
     }
-    while (fp1 != EOF || fp2 != EOF)
+    while (!feof(fp1) || !feof(fp2))
     {
         if (fgetc(fp1) != fgetc(fp2))
         {
@@ -16,7 +18,7 @@ int main()
             break;
         }
     }
-    if(flag==0)
+    if (flag == 0)
         printf("\nboth file are same");
     else
         printf("\nnot same file ");
