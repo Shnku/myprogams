b1512f5772b30312e3cac2477163c7991d8068a7
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-02-18 20:22:22 +0530

    .

---
+++ collage/c/array string struct c/operations_on_string_menu_driven.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int calc_strlength(char *str);
+void show_address_of_each_character(char *ch);
+void concat_manual(char *ch,char ch2);
+void concat_libray_func(char *ch1,char *ch2);
+void string_cpmpare(char *c1,char* c2);
+
+
+
+int main()
+{
+    char c[50];
+    printf("\nenter the syrind: ");
+    fgets(c,50,stdin);
+
+    return 0;
+}
--- update_git.c
+++ update_git.c
@@ -5,13 +5,16 @@ int main()
     printf("\n....................................");
     printf("\nupdatine the git repo\n");
     printf("....................................\n");
-    
+
     system("git add --all");
     printf("\n\n");
     system("git commit -m \".\"");
     printf("\n\n");
     system("git push -u origin");
 
+    printf("\n....................................");
+    printf("\nupdating done..\n");
+    printf("....................................\n");
+
     return 0;
 }
-
