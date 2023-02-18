01c136e108cd436774ca7e8087bc1db9b07747d2
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-02-18 23:19:35 +0530

    .

--- collage/c/array string struct c/operations_on_string_menu_driven.c
+++ collage/c/array string struct c/operations_on_string_menu_driven.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
+void get_string(char *s);
 int calc_strlength(char *str);
 void show_address_of_each_character(char *ch);
-void concat_manual(char *ch,char ch2);
-void concat_libray_func(char *ch1,char *ch2);
-void string_cpmpare(char *c1,char* c2);
-
-
+void concat_manual(char *ch, char ch2);
+void concat_libray_func(char *ch1, char *ch2);
+void string_cpmpare(char *c1, char *c2);
+void convert_to_upper(char *c);
+void convert_to_lower(char *c);
 
 int main()
 {
     char c[80];
     printf("\nenter the syrind: ");
-    fgets(c,80,stdin);
-
+    fgets(c, 80, stdin);
+    printf("%c", stdin);
+    
     return 0;
 }
--- collage/c/array string struct c/string_intro_prblm.c
+++ collage/c/array string struct c/string_intro_prblm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int th_2ndmain()
 {
@@ -6,7 +7,7 @@ int th_2ndmain()
     printf("\nsizeo is %d", sizeof(string));
     printf("\nenter the string: ");
     // scanf("%s",string);//only takes before whitespace...
-    gets(string);
+    // gets(string);
     /*
     It looks like you encountered a segmentation fault ( SIGSEGV ). This is generally caused by a program trying to write or read memory addresses that it does not have the permission to access. It could be caused by an incorrect pointer, undefined behavior or using uninitialized memory for example.
     */
@@ -24,6 +25,84 @@ int th_2ndmain()
     return 0;
 }
 
-int main(){
-    
+int main_a()
+{
+    int no;
+    printf("\nen the no: ");
+    scanf("%d", &no);
+
+    printf("%c", stdin);
+
+    char c[80];
+    printf("\nenter the syrind: ");
+    fgets(c, 80, stdin);
+    printf("\nthe string is: %s", c);
+
+    /*
+    !first case,...
+    en the no: 56 ljgmdgbngff //?then '\n'
+    � //? this is actually '\n' character
+    enter the syrind:
+    the string is:  ljgmdgbngff
+    */
+
+    /*
+    !second case...
+    en the no: 35 //?then '\n'
+    � //? this is actually '\n' character
+    enter the syrind: //? it reads the the '\n' and exits..
+    the string is:
+    */
+}
+
+int main_onlychar()
+{
+    char ch[50];
+    char ch2[60];
+
+    printf("%c", stdin);
+    printf("\nen the first: ");
+    fgets(ch, 50, stdin);
+    printf("%c", stdin);
+    printf("\nen the second: ");
+    fgets(ch2, 60, stdin);
+    printf("%c", stdin);
+
+    printf("\n\nenterd strings are:\n%s\n%s", ch, ch2);
+    printf("\nsizeos(ch): %d", strlen(ch));
+    printf("\nsizeos(ch2): %d", strlen(ch2));
+
+    return 0;
+
+    /*
+    !this is not causing problem at all  ....
+    �
+    en the first: ghjghjhh
+    �
+    en the second: hjgjg
+    �
+
+    enterd strings are:
+    ghjghjhh
+
+    hjgjg
+
+    sizeos(ch): 9
+    sizeos(ch2): 6
+    */
+}
+
+int main()
+{
+    // fixinf the before problem occures in ...*main_a*..
+    int no;
+    printf("\nen the no: ");
+    scanf("%d", &no);
+
+    printf("%c", stdin);
+    getchar(); // it will read the '\n'
+    char c[80];
+    printf("\nenter the syrind: ");
+    fgets(c, 80, stdin);
+    printf("\nthe string is: %s", c);
 }
