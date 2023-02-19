82a4cae96fed5da57f0843db2321b4e61a73ef48
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-02-19 13:17:45 +0530

    .

--- collage/c/array string struct c/operations_on_string_menu_driven.c
+++ collage/c/array string struct c/operations_on_string_menu_driven.c
@@ -1,5 +1,6 @@
 // program for all string operations....
 #include <stdio.h>
+#include <string.h>
 
 void get_string(char *s);
 int calc_strlength(char *str);
@@ -14,8 +15,35 @@ int main()
 {
     char c[80];
     printf("\nenter the syrind: ");
-    fgets(c, 80, stdin);
+    // fgets(c, 80, stdin);
+    get_string(c);
     printf("%c", stdin);
+    printf("\n%s", c);
 
     return 0;
+}
+
+/*
+this is the get string function....*/
+void get_string(char *s)
+{
+    char ch;
+    for (int i = 0; i < 80; i++)
+    {
+        ch = getchar();
+        // getc(ch); //!giving error ..
+        switch (ch)
+        {
+        case '\n':
+            s[i] = '\0';
+            return; //! not break;
+        case '\b':
+            if (i > 0)
+                i++;
+            break;
+        default:
+            s[i] = ch;
+        }
+    }
+    s[79] = '\0';
 }
