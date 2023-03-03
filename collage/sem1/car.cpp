3400fe49c0a0beaf979919ca5f8b197eaf38dc1b
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-03 01:46:02 +0000

    .

--- collage/sem1/car.cpp
+++ collage/sem1/car.cpp
@@ -5,8 +5,8 @@ using namespace std;
 class car
 {
 	char nm[20];
-	int l,w,h;
-	float price,tax;
+	int l, w, h;
+	float price, tax;
 
 public:
 	void getdeta()
@@ -21,34 +21,32 @@ public:
 
 	void calc_tax()
 	{
-		if (l >12)
-			tax = price * 14/100;
+		if (l > 12)
+			tax = price * 14 / 100;
 		else
-			tax = price * 10/100;
+			tax = price * 10 / 100;
 	}
 
 	void display()
 	{
-		cout << "\nCAR NAME: "<< nm
-			 << "\t " <<l <<" * "<<w<<" * "<<h
-			 << "\tPrice: "<< price << "\t So Onroad tax = "<< tax
-			 << "\tSo Final Price:= "<< price+tax <<endl ;
+		cout << "\nCAR NAME: " << nm
+			 << "\t " << l << " * " << w << " * " << h
+			 << "\tPrice: " << price << "\t So Onroad tax = " << tax
+			 << "\tSo Final Price:= " << price + tax << endl;
 	}
 };
 
 int main()
 {
-	car c1,c2;
+	car c1, c2;
 
 	c1.getdeta();
 	c1.calc_tax();
 	c1.display();
-	
+
 	c2.getdeta();
 	c2.calc_tax();
 	c2.display();
 
 	return 0;
 }
-
-
