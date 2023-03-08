f239e8af657ec31f3e051d5021e0338556f8f0bf
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-08 16:27:33 +0530

    .

--- collage/c/pointers and file malloc/file_editations.c
+++ collage/c/pointers and file malloc/file_editations.c
@@ -1,9 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void file_open(FILE *fp, char nm[10], char mod[2]);
+void remove_whitespace(FILE *);
+void sentense_case_string(FILE *);
+void file_word_search(FILE *);
 
 int main()
 {
-    
-
+    FILE *p;
+    file_open(p, "file3.txt", "r");
+    file_word_search(p);
     return 0;
 }
+
+void file_open(FILE *fp, char nm[10], char mod[2])
+{
+    if ((fp = fopen(nm, mod)) == NULL)
+    {
+        printf("\ncant open file");
+        exit(0);
+    }
+    printf("\nfile %s open in %s mode", nm, mod);
+}
+
+/*
+!wrong.....
+The while loop could potentially continue forever since feof() may not trigger until after a read operation has been attempted past the end of file.
+Inside the while loop, the code is calling fgetc() twice, so it will be reading every other character.
+The strcmp() function should use == instead of ? to assign the value to the flag variable.
+*/
+// /*
+void file_word_search(FILE *fp)
+{
+    char word[20], ch;
+    int flag = 0;
+    printf("\nen the word to search: ");
+    fgets(word, 20, stdin);
+    while ((ch=fgetc(fp)) != EOF)
+    {
+        int i = 0;
+        char temp[12];
+        while ((ch = fgetc(fp)) != ' ')
+        {
+            temp[i++] = ch; // using fgetc()..reads 2nd character..
+        }
+        temp[i] = '\n';
+        strcmp(temp, word) ? (flag = 1) : (flag = 0);
+    }
+    flag ? printf("\nnot match") : printf("\nmatch");
+}
+// * /
--- collage/c/pointers and file malloc/student_data_file.c
+++ collage/c/pointers and file malloc/student_data_file.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct student
+{
+    int roll;
+    char name[30];
+    char class[10];
+    int year;
+    float total_marks;
+};
+typedef struct student stu;
+void get_student_data(stu *);
+void display_student_deta(stu);
 
 int main()
 {
+    stu a;
+    FILE *fp = NULL;
+    get_student_data(&a);
+    display_student_deta(a);
 
+    if (fp = fopen("file3.txt", "w") == NULL)
+    {
+        printf("\nfile can't be open ");
+        exit(0);
+    }
+    
     return 0;
+}
+
+void get_student_data(stu *s1)
+{
+    printf("\nEnter student roll, name : ");
+    scanf("%d", &s1->roll);
+    fgets(s1->name, 30, stdin);
+    printf("enter student class, year & marks: ");
+    fgets(s1->class, 10, stdin);
+    scanf("%d%f", &s1->year, &s1->total_marks);
+}
+
+void display_student_deta(stu s1)
+{
+    printf("\nStudent Data:----\n%d, %s", s1.roll, s1.name);
+    printf("class: %s year: %d, total marks: %f", s1.class, s1.year, s1.total_marks);
 }
