dff86d23441b0509dd71ab17def5f038c77f2142
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-21 16:57:19 +0530

    .

--- collage/c/array string struct c/array_shifting.c
+++ collage/c/array string struct c/array_shifting.c
@@ -4,42 +4,43 @@
 
 void shift_array_left(int *, int);
 void shift_array_right(int *, int, int);
-void getdata(int a[], int size);
-void display(int *a, int size);
+void getdata(int a[], int len);
+void display(int *a, int len);
 
 int main()
 {
-    int *array, size;
-    printf("\nenter the array size: ");
-    scanf("%d", &size);
-    array = (int *)malloc(size * sizeof(int));
-    getdata(array, size);
+    int *array, len;
+    printf("\nenter the array len: ");
+    scanf("%d", &len);
+    array = (int *)malloc(len * sizeof(int));
+    getdata(array, len);
     printf("\nbefore : ");
-    display(array, size);
-    shift_array_left(array, size);
+    display(array, len);
+    // shift_array_left(array, len);
+    // printf("\nafter: ");
+    // display(array, len);
+    printf("\nnow right shift 2----\n");
+    shift_array_right(array, len, 2);
     printf("\nafter: ");
-    display(array, size);
-    shift_array_right(array, size, 2);
-    printf("\nafter: ");
-    display(array, size);
+    display(array, len);
     return 0;
 }
 
-void getdata(int a[], int size)
+void getdata(int a[], int len)
 {
     printf("\nenter array elements:\n");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < len; i++)
         scanf("%d", &a[i]);
 }
 
-void display(int *a, int size)
+void display(int *a, int len)
 {
     printf("\ndisplaying the array: ");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < len; i++)
         printf(" %d ", a[i]);
 }
 
-void shift_array_left(int *a, int size)
+void shift_array_left(int *a, int len)
 {
     int shift_count;
     printf("\nen ter the no of shift: ");
@@ -47,29 +48,33 @@ void shift_array_left(int *a, int size)
     while (shift_count)
     {
         int temp = a[0];
-        for (int i = 0; i < size - 1; i++)
+        for (int i = 0; i < len - 1; i++)
         {
             printf("\na[%d] <- a[%d] // %d -< %d", i, i + 1, a[i], a[i + 1]);
             a[i] = a[i + 1];
         }
-        printf("\na[%d-1]=%d", size, temp);
-        a[size - 1] = temp;
+        printf("\na[%d-1]=%d", len, temp);
+        a[len - 1] = temp;
         shift_count--;
     }
 }
 
-void shift_array_right(int *a, int size, int count)
+void shift_array_right(int *a, int len, int count)
 {
     while (count)
     {
-        int temp = a[size - 1];
-       // for (int i = 1; i < size - 1; i++)
-        for (int i=size; i>0; i--)
+        int temp = a[len - 1];
+        // for (int i = len - 2; i >= 0; i--)
+        // {
+        //     printf("\na[%d] <- a[%d] // %d -< %d", i + 1, i, a[i + 1], a[i]);
+        //     a[i + 1] = a[i];
+        // }
+        for (int i = len - 1; i >= 0; i--)
         {
-            printf("\na[%d] <- a[%d] // %d -< %d", i, i + 1, a[i], a[i - 1]);
+            printf("\na[%d] <- a[%d] // %d -< %d", i, i - 1, a[i], a[i - 1]);
             a[i] = a[i - 1];
         }
-        printf("\na[%d-1]=%d", size, temp);
+        printf("\na[0]=%d", temp);
         a[0] = temp;
         count--;
     }
@@ -78,7 +83,7 @@ void shift_array_right(int *a, int size, int count)
 /*....complete  ....*/
 /*output*/
 /*
-enter the array size: 5
+enter the array len: 5
 enter array elements: 10 20 30 40 50
 
 before :
@@ -108,4 +113,24 @@ a[1] = 40
 a[2] = 50
 a[3] = 10
 a[4] = 20
+*/
+
+/*right shifting ,,*/
+/*
+before :
+displaying the array:  10  20  30  40  50
+now right shift 2----
+
+a[4] <- a[3] // 50 -< 40
+a[3] <- a[2] // 40 -< 30
+a[2] <- a[1] // 30 -< 20
+a[1] <- a[0] // 20 -< 10
+a[0]=50
+a[4] <- a[3] // 40 -< 30
+a[3] <- a[2] // 30 -< 20
+a[2] <- a[1] // 20 -< 10
+a[1] <- a[0] // 10 -< 50
+a[0]=40
+after:
+displaying the array:  40  50  10  20  30
 */
---
+++ collage/c/array string struct c/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+    // shift_array_left(array, len);
+    // printf("\nafter: ");
+    // display(array, len);
