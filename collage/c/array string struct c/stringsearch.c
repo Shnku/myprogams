a9c0616dfb8263b1e771f5b23044edd0c93dac7b
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-25 23:07:54 +0530

    .

--- collage/c/array string struct c/stringsearch.c
+++ collage/c/array string struct c/stringsearch.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-int string_search(char *str, char *word);
+void string_search(char *str, char *word);
 
 int main()
 {
     char string[50], word[10];
-    int flag = 0;
+    // int flag = 0;
 
     printf("\nenter the string: ");
     fgets(string, 50, stdin);
     printf("\nenter the word to search: ");
-    fgets(word, 10, stdin);
-
-    flag = string_search(string, word);
-    printf("\n%s", flag ? "found" : "not found");
+    fgets(word, sizeof(word), stdin);
+    word[strcspn(word, "\n")] = '\0';
+    string_search(string, word);
+    // printf("\n%s", flag ? "found" : "not found");
 
     return 0;
 }
 
-int string_search(char *str, char *word)
+void string_search(char *str, char *word)
 {
     int i = 0;
     char temp[10];
     while (str[i] != '\0')
     {
         int j = 0;
+
         while (1)
         {
             if (str[i] == ' ' || str[i] == '\0')
@@ -33,68 +34,27 @@ int string_search(char *str, char *word)
             temp[j++] = str[i++];
         }
         temp[j] = '\0';
-        printf("\ntemp=%s", temp);
-    }
-}
 
+        printf("\n__temp=%s", temp);
+        printf("\n__word=%s", word);
 
-
-/*
- this is wrong code .
-initialization of 'char' from 'char *' makes integer from pointer without a cast [-Wint-conversion]
-    7 |     char s2 = "the";.
- passing argument 2 of 'strcmp' makes pointer from integer without a cast [-Wint-conversion]
-   28 |         if (strcmp(t,s2)==0)
-      |                      ^~
-      |                      |
-      |                      char
-expected 'const char *' but argument is of type 'char'
-  156 | n int strcmp (const char *__s1, const char *__s2)
-      |                                 ~~~~~~~~~~~~^~~~
-
-
-en the sentense: this is th e sn
-
-input succesful
-
-*/
-/*
-int main_error()
-{
-    char s1[100], t[20];
-    char s2 = "the";
-    printf("\nen the sentense: ");
-    scanf("%[^\n]", s1);
-    printf("\ninput succesful\n");
-    int i = 0, j;
-    while (s1[i] != '\0')
-    {
-        j = 0;
-        while (1)
+        if (strcmp(word, temp) == 0) // only last line compared
         {
-            if (s1[i] != '\0' || s1[i] != ' ')
-            {
-                t[j] = s1[i];
-                printf("\ni=%d, j=%d  ", i, j);
-                j++;
-                i++;
-            }
+            printf("\ncomplring %s & %s", temp, word);
+            printf("\nword found\n");
             break;
-            t[j] = '\0';
         }
-        if (strcmp(t, s2) == 0)
-        {
-            printf("found");
-        }
-        printf("\tt=%s", t);
+        else
+            printf("\nnot found\n");
+        i++;
     }
-
-    return 0;
 }
 
-// printf("\n......");
-// if (s1[i] == '\0' || s1[i] == ' ')
-//     printf("\ni=%d j=%d ",i,j);
-//     break;
-
+/*
+what  is happenning ...
+!the problem happens with fgets()....it reada the '\n' at end.
+?thats why,
+complring the
+             & the
+?this is printed like this....
 */
