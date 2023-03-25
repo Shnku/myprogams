340909c863a00246072891dfe36b428f6557bab7
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-25 13:45:30 +0530

    .

--- collage/c/array string struct c/sprat_word_fonstr.c
+++ collage/c/array string struct c/sprat_word_fonstr.c
@@ -1,24 +1,73 @@
+/*program to saperate word from a string...*/
 #include <stdio.h>
 
 int main()
 {
-    char s[100],c[20];
+    char s[100], w[20];
     printf("\nen the string: ");
-    scanf("%[^\n]", s);
-    int i=0,j,k;
-
-    while (s[i]!='\0')
+    fgets(s, 100, stdin);
+    int k = 0, i = 0;
+    while (s[k])
     {
-        j=0;k=0;
-        if (s[j]==' ' || s[j]=='\0')
+        int j = 0;
+        while (s[i])
         {
-            c[k]=s[j];
-            printf("\n %c",c[k]);
-            j++;
-            k++;
-            // c[j]='\0';     
+            if (s[i] == ' ' || s[i] == '\0')
+                break;
+            printf("\ntemp[%d]=%c", j, s[i]);
+            w[j++] = s[i++];
         }
-        i++;
+        printf("\n");
+        k++;
     }
     return 0;
 }
+
+/*____explanation____for one while only__*/
+/*
+    while (s[i])
+    {
+        if (s[i] == ' ' || s[i] == '\0')
+            break;
+        printf("\ntemp[%d]=%c", j, s[i]);
+        temp[j++] = s[i++];
+    }
+enter the string: this is the
+temp[0]=t
+temp[1]=h
+temp[2]=i
+temp[3]=s
+
+then it breaks as ' ' is found ...
+*/
+
+/*this is alsio wrong..........*/
+/*
+    while (s[i] != '\0')
+    {
+        int j = 0;
+        while (s[i] != ' ' && s[i] != '\0')
+        {
+            w[j++] = s[i++];
+        }
+        w[j] = '\0';
+        printf("first word is: %s\n", w);
+    }
+first word is:
+first word is:
+first word is:
+first word is:
+first word is:
+first word is:
+first word is:
+first word is:
+^C⏎
+/*!
+!problem is*/
+/*
+The problem with this program is that it only prints out the first word in the input string. This is because the inner while loop that extracts each word from the string is nested inside a while loop that iterates over each character in the string.
+
+So the inner while loop runs to extract the first word, but then the outer while loop continues from where the inner loop left off, and since the variable j is not reset, the second word will be placed immediately after the first word in the w array, overwriting it. Then the program prints out the first word again, because w still contains the first word.
+
+To fix this issue, you need to create a 2D array to store all of the words in the string, and increment a separate index variable for each new word. Here's an updated version of your program that does this:
+*/
--- collage/c/array string struct c/stringsearch.c
+++ collage/c/array string struct c/stringsearch.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 #include <string.h>
+int string_search(char *str, char *word);
 
 int main()
+{
+    char string[50], word[10];
+    int flag = 0;
+
+    printf("\nenter the string: ");
+    fgets(string, 50, stdin);
+    printf("\nenter the word to search: ");
+    fgets(word, 10, stdin);
+
+    flag = string_search(string, word);
+    printf("\n%s", flag ? "found" : "not found");
+
+    return 0;
+}
+
+int string_search(char *str, char *word)
+{
+    int i = 0;
+    char temp[10];
+    while (str[i] != '\0')
+    {
+        int j = 0;
+        while (1)
+        {
+            if (str[i] == ' ' || str[i] == '\0')
+                break;
+            printf("\ntemp[%d]=%c", j, str[i]);
+            temp[j++] = str[i++];
+        }
+        temp[j] = '\0';
+        printf("\ntemp=%s", temp);
+    }
+}
+
+
+
+/*
+ this is wrong code .
+initialization of 'char' from 'char *' makes integer from pointer without a cast [-Wint-conversion]
+    7 |     char s2 = "the";.
+ passing argument 2 of 'strcmp' makes pointer from integer without a cast [-Wint-conversion]
+   28 |         if (strcmp(t,s2)==0)
+      |                      ^~
+      |                      |
+      |                      char
+expected 'const char *' but argument is of type 'char'
+  156 | n int strcmp (const char *__s1, const char *__s2)
+      |                                 ~~~~~~~~~~~~^~~~
+
+
+en the sentense: this is th e sn
+
+input succesful
+
+*/
+/*
+int main_error()
 {
     char s1[100], t[20];
     char s2 = "the";
@@ -17,15 +75,14 @@ int main()
             if (s1[i] != '\0' || s1[i] != ' ')
             {
                 t[j] = s1[i];
-                printf("\ni=%d, j=%d  ",i,j);
+                printf("\ni=%d, j=%d  ", i, j);
                 j++;
                 i++;
             }
             break;
             t[j] = '\0';
-            
         }
-        if (strcmp(t,s2)==0)
+        if (strcmp(t, s2) == 0)
         {
             printf("found");
         }
@@ -35,7 +92,9 @@ int main()
     return 0;
 }
 
-            // printf("\n......");
-            // if (s1[i] == '\0' || s1[i] == ' ')
-            //     printf("\ni=%d j=%d ",i,j);
-            //     break;
+// printf("\n......");
+// if (s1[i] == '\0' || s1[i] == ' ')
+//     printf("\ni=%d j=%d ",i,j);
+//     break;
+
+*/
