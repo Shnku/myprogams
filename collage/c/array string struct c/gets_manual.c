503e12d16ab3c037b5fb9f67015306260a483dd2
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-25 23:20:39 +0530

    .

--- collage/c/array string struct c/gets_manual.c
+++ collage/c/array string struct c/gets_manual.c
@@ -3,13 +3,6 @@
 #include <string.h>
 
 void get_string(char *s);
-int calc_strlength(char *str);
-void show_address_of_each_character(char *ch);
-void concat_manual(char *ch, char ch2);
-void concat_libray_func(char *ch1, char *ch2);
-void string_cpmpare(char *c1, char *c2);
-void convert_to_upper(char *c);
-void convert_to_lower(char *c);
 
 int main()
 {
