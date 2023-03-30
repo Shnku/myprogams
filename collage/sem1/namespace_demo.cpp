0dbe54ab4a58ed99d48846767b8e2c7193e21878
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-30 16:56:01 +0530

    .

--- collage/sem1/namespace_demo.cpp
+++ collage/sem1/namespace_demo.cpp
@@ -13,9 +13,25 @@ namespace B
     void printx() { cout << x; }
 } // namespace B
 
+namespace my_ns = A;
+namespace your_ns = B;
+
 int main()
 {
     A::printx();
+    cout << my_ns::x << endl;
     B::printx();
+
+    cout << "\n__now using ___\n";
+    using namespace A;
+    printx();
+
+    using namespace your_ns;
+    // printx(); // gives ambiguious error,,
+    /* This is because the using directive brings all the symbols of the namespace into the current scope and can lead to naming conflicts.*/
+
+    using your_ns::printx; // solution..
+    printx();
+
     return 0;
 }
