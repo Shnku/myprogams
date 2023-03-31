be1b1deeed3c63d5499832b9b3603f24f6564a92
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-31 15:34:34 +0530

    .

--- collage/c/array string struct c/array_2nd_max.c
+++ collage/c/array string struct c/array_2nd_max.c
@@ -3,16 +3,54 @@
 #define size 5
 
 int find_2nd_max(int *arr, int len);
-
+void swap(int *, int *);
 
 int main()
 {
     int array[size];
-    int max_2nd;
+    int mAx_2;
     get_array_data(array, size);
-    max_2nd = find_2nd_max(array, size);
-    printf("\nthe 2nd max value of the array is: %d", max_2nd);
+    mAx_2 = find_2nd_max(array, size);
+    printf("\nthe 2nd max value of the array is: %d", mAx_2);
     return 0;
 }
 
+int find_2nd_max(int arr[], int n)
+{
+    int mx_1 = arr[0];
+    int mx_2 = arr[1];
+    if (mx_1 < mx_2)
+    {
+        swap(&mx_1, &mx_2);
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (mx_1 < arr[i])
+        {
+            mx_2 = mx_1;
+            mx_1 = arr[i];
+        }
+        if (mx_2 < mx_1 && mx_2 < arr[i])
+        {
+            mx_2 = arr[i];
+        }
+    }
+    return mx_2;
+}
 
+void swap(int *x, int *y)
+{
+    int t = *x;
+    *x = *y, *y = t;
+}
+
+/*
+_ENTER THE ARRAY ELEMENTS___
+1
+5
+44
+2
+55
+
+the 2nd max value of the array is: 55
+*/
