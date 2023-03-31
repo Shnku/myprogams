cdb94e7ac7b88c5bae24d52280e4dc083c3a8da2
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-31 16:26:10 +0530

    .

--- collage/c/array string struct c/array_frequency.c
+++ collage/c/array string struct c/array_frequency.c
@@ -2,33 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "get_put_array.h"
-#define length 5
+#define SIZE 5
 
-int store_frequency(int *a, int len, int *temp, int *count);
+int store_frequency(int *arr, int len, int *temp, int *count);
 
 int main()
 {
-    int a[length];
-    int t[length], c[length];
-    get_array_data(a, length);
-    int size = store_frequency(a, length, t, c);
-    display_array(t, size);
-    display_array(c, size);
+    int arr[SIZE];
+    int tmp[SIZE], cnt[SIZE];
+    get_array_data(arr, SIZE);
+    int size = store_frequency(arr, SIZE, tmp, cnt);
+    display_array(tmp, size);
+    display_array(cnt, size);
 
     return 0;
 }
 
-int store_frequency(int *a, int len, int *temp, int *count)
+int store_frequency(int *arr, int len, int *temp, int *count)
 {
     // int *temp,*count;
     int point = 0, flag = 0;
     for (int i = 0; i < len; i++)
     {
-        temp[point] = a[i];
+        temp[point] = arr[i];
         count[point] = 1;
         for (int j = 0; j < point; j++)
         {
-            if (temp[j] == a[i])
+            if (temp[j] == arr[i])
             {
                 count[j]++;
                 flag = 1;
