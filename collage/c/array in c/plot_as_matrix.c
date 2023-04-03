9503a05f5011a6be64ac5b40127fccf255bdb530
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-03 20:15:40 +0530

    .

--- collage/c/array in c/plot_as_matrix.c
+++ collage/c/array in c/plot_as_matrix.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #define max 10
 
-int box_draw[max][max];
-void ploat_graph(int **arr);
+typedef struct matrix_box
+{
+    int box_draw[max][max];
+    int row, col;
+} box;
+
+void ploat_graph(box, int *);
 
 int main()
 {
+    box b1;
+    int col = 4, row = 4;
+    b1.row = row, b1.col = col;
+    b1.box_draw[row][col];
 
+    int arr[] = {1, 5, 3, 4};
+    ploat_graph(b1, arr);
     return 0;
 }
 
-void ploat_graph(int **arr)
+void ploat_graph(box B, int *data)
 {
-    
+    for (int i = 0; i < B.row; i++)
+    {
+        for (int k = 0; k < data[i]; k++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
 }
