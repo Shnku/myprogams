a8166650748a9a91f44a7a6e151c1cfc8a94d5cc
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-04 12:10:17 +0530

    .

---
+++ collage/cpp/oops/shape_class_functions.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class shape2d
+{
+private:
+    int height, width;
+
+public:
+    shape2d() {}
+    ~shape2d() {}
+    shape2d(int h, int w)
+    {
+        height = h;
+        width = w;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
