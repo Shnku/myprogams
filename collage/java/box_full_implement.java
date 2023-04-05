c043861272e6ccdfa5b0cb07f74e070a82ec00b4
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-05 20:10:40 +0530

    .

---
+++ collage/java/box_full_implement.java
@@ -0,0 +1,15 @@
+// import boxdemo.box;  //!not possible...as it has main method..
+
+/**
+ * box
+ */
+class box {
+
+    
+}
+
+public class box_full_implement {
+    public static void main(String[] args) {
+
+    }
+}
