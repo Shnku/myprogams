660e0b609742d20eb65a354213a3f32161664a7a
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-05 20:34:13 +0530

    .

--- collage/java/box_full_implement.java
+++ collage/java/box_full_implement.java
@@ -4,9 +4,29 @@
  * box
  */
 class box {
+    int height, lehgth, width;
 
-    
+    box() {
+        height = lehgth = width = 0;
+    }
+
+    box(int lehgth, int width, int height) {
+        this.height = height;
+        this.lehgth = lehgth;
+        this.width = width;
+    }
+
+    box(int len) {
+        height = width = lehgth = len;
+    }
+
+    float volume() {
+        return height * lehgth * width;
+    }
+
 }
+
+
 
 public class box_full_implement {
     public static void main(String[] args) {
