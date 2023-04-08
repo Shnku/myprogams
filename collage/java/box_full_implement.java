8818cb9e5d22162a9ad7c9dfbfc29b43877fa142
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-08 19:28:31 +0530

    .

--- collage/java/box_full_implement.java
+++ collage/java/box_full_implement.java
@@ -20,32 +20,39 @@ class box {
         height = width = lehgth = len;
     }
 
-    float volume() {
+    double volume() {
         return height * lehgth * width;
     }
 
 }
 
 class box_with_weight extends box {
-    float weight;
+    double weight;
 
-    box_with_weight(int l, int wd, int h, float w) {
+    box_with_weight(int l, int wd, int h, double w) {
         super(l, wd, h);
         weight = w;
     }
 }
 
 class box_package extends box_with_weight {
-    float sipping_price;
+    double tax;
 
-    box_package(int l, int wd, int h, float w, float p) {
+    box_package(int l, int wd, int h, double w, double p) {
         super(l, wd, h, w);
-        sipping_price = p;
+        tax = p;
+    }
+
+    double shipping_price_is() {
+        return weight * tax;
     }
 }
 
 public class box_full_implement {
     public static void main(String[] args) {
+        box_package obj = new box_package(4, 5, 6, 7, 10.2);
+
+        System.out.println(obj.shipping_price_is());
 
     }
 }
