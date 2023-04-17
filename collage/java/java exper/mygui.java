5543cf4430f6fd43588d0fea21486dc6e629f6e1
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-17 16:55:04 +0530

    .

--- collage/java/java exper/mygui.java
+++ collage/java/java exper/mygui.java
@@ -12,13 +12,15 @@ public class mygui {
         frame.setSize(400, 200);
 
         JButton button1 = new JButton("<press this button>");
-        frame.add(button1);
+        button1.setBounds(150, 40, 120, 10);
+        frame.add(button1);// .setSize(50, 30); //seted via setbound
         JButton button2 = new JButton("<2nd button>");
+        button2.setSize(150, 30);
         frame.add(button2);
         // frame.getContentPane().add(button1).setSize(150, 30);
         // frame.getContentPane().add(button2);
 
-        
+        frame.setLayout(null); // no defaust layout manager..
         frame.setVisible(true);
     }
 }
