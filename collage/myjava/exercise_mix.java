f3505dce92470742fe020f5b7af71a5c2db52012
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-30 17:08:21 +0530

    .

--- collage/myjava/exercise_mix.java
+++ collage/myjava/exercise_mix.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 class quardratic_eq {
     double c, b, a;
@@ -43,22 +44,30 @@ public class exercise_mix {
 
     public static void main(String[] args) {
 
+        /* _________quardratic_eq solver__________ */
         quardratic_eq myequation = new quardratic_eq(1, 5, 6);
-        double arr_of_roots[] = myequation.find_root();
+        double root_ar[] = myequation.find_root();
         /*
-         * //using for each loop/.....
-         * for (double d : arr_of_roots) {
+         * for (double d : root_ar) {
          * System.out.println("the roots are: " + d);
          * }
          */
-        for (int i = 0; i < arr_of_roots.length; i++) {
-            System.out.println("the " + (i + 1) + "st root is: " + arr_of_roots[i]);
+        for (int i = 0; i < root_ar.length; i++) {
+            System.out.println("the " + (i + 1) + "st root is: " + root_ar[i]);
             // if not using (i+1) the it put them differently...
         }
 
-        CP_SP_exercise mysell = new CP_SP_exercise(45000, 55000);
-        System.out.printf("net : %.2f %%", mysell.profit_loss());
-        // printf means formated output....
+        /* ______profit & loss calculator__________ */
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter cost_price & selling_price: ");
+            double cost = input.nextDouble();
+            double sell = input.nextDouble();
+            CP_SP_exercise mysell = new CP_SP_exercise(cost, sell);
+            System.out.printf("net : %.2f %%", mysell.profit_loss());
+            // printf means formated output....
+        } catch (Exception e) {
+            System.out.println("ERROR INPUT TYPE__....");
+        }
 
     }
 }
@@ -80,4 +89,13 @@ public class exercise_mix {
  * the 1st root is: -2.0
  * the 2st root is: -3.0
  * net : 22.22%
+ * 
+ * 
+ * javac exercise_mix.java && java exercise_mix
+ * the 1st root is: -2.0
+ * the 2st root is: -3.0
+ * Enter cost_price & selling_price:
+ * lhj
+ * ERROR INPUT TYPE__....
+ * 
  */
---
+++ collage/myjava/quardratic_extended_main.java
@@ -0,0 +1,17 @@
+import quardratic_eq;
+
+class quardratic_cheaking extends quardratic_eq {
+
+    quardratic_cheaking(double a, double b, double c) {
+        super(a, b, c);
+
+    }
+
+}
+
+public class quardratic_extended_main {
+
+    public static void main(String[] args) {
+
+    }
+}
