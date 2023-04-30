f01da8fb18ca62e3ca0511bb4654124a470f8077
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-04-30 16:41:00 +0530

    .

--- collage/java/exercise_mix.java
+++ collage/java/exercise_mix.java
@@ -16,11 +16,11 @@ class quardratic_eq {
     }
 }
 
-class CP_SP {
+class CP_SP_exercise {
     double cost, sell;
     double profit, loss;
 
-    CP_SP(double cost, double sell) {
+    CP_SP_exercise(double cost, double sell) {
         this.cost = cost;
         this.sell = sell;
     }
@@ -28,10 +28,10 @@ class CP_SP {
     double profit_loss() {
         if (sell < cost) {
             loss = cost - sell;
-            return loss;
+            return loss / cost * 100;
         } else {
             profit = sell - cost;
-            return profit;
+            return profit / cost * 100;
         }
     }
 }
@@ -42,8 +42,8 @@ class CP_SP {
 public class exercise_mix {
 
     public static void main(String[] args) {
-        quardratic_eq myequation = new quardratic_eq(1, 5, 6);
 
+        quardratic_eq myequation = new quardratic_eq(1, 5, 6);
         double arr_of_roots[] = myequation.find_root();
         /*
          * //using for each loop/.....
@@ -53,11 +53,12 @@ public class exercise_mix {
          */
         for (int i = 0; i < arr_of_roots.length; i++) {
             System.out.println("the " + (i + 1) + "st root is: " + arr_of_roots[i]);
+            // if not using (i+1) the it put them differently...
         }
-        // if not using (i+1) the it put them differently...
 
-        CP_SP mysell = new CP_SP(45000, 55000);
-        System.out.println("net : " + mysell.profit_loss());
+        CP_SP_exercise mysell = new CP_SP_exercise(45000, 55000);
+        System.out.printf("net : %.2f %%", mysell.profit_loss());
+        // printf means formated output....
 
     }
 }
@@ -74,4 +75,9 @@ public class exercise_mix {
  * the roots are: -2.0
  * the roots are: -3.0
  * net : 10000.0
+ * 
+ * 
+ * the 1st root is: -2.0
+ * the 2st root is: -3.0
+ * net : 22.22%
  */
