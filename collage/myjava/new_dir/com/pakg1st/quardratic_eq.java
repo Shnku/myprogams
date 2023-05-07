3bd2cfcde6c1ec6d06bae6602b6a994c2ef44a59
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-05-07 12:05:45 +0530

    .

--- collage/myjava/new_dir/com/pakg1st/quardratic_eq.java
+++ collage/myjava/new_dir/com/pakg1st/quardratic_eq.java
@@ -1,10 +1,12 @@
 package com.pakg1st;
 
 public class quardratic_eq {
-    protected double c, b, a;
+    protected  double c;
+    protected  double b;
+    protected  double a;
 
     public quardratic_eq(double a, double b, double c) {
-        this.c = c;
+        this.c = c; // static way..
         this.b = b;
         this.a = a;
     } // there is no default argument in java...
@@ -16,3 +18,16 @@ public class quardratic_eq {
         return root;
     }
 }
+
+/*
+ * quardratic_eq.c = c; // static way..
+ * quardratic_eq.b = b;
+ * quardratic_eq.a = a;
+ * 
+ */
+
+// protected static double c;
+// protected static double b;
+// protected static double a;
+
+//here naming collition happenning withs extended_quard_eq program...
--- collage/myjava/new_dir/com/pkg2/extended_quard_eq.java
+++ collage/myjava/new_dir/com/pkg2/extended_quard_eq.java
@@ -1,6 +1,7 @@
 
 package com.pkg2;
 
+import java.util.Scanner;
 import com.pakg1st.quardratic_eq;
 
 public class extended_quard_eq extends quardratic_eq {
@@ -29,25 +30,76 @@ public class extended_quard_eq extends quardratic_eq {
 
     public static void main(String[] args) {
 
-        extended_quard_eq foo = new extended_quard_eq(4, 5, 6);
-        int retval = foo.has_root();
-        if (retval == 1) {
-            System.out.println("root is real & not equal__");
-            double[] x = foo.find_root();
-            for (double d : x) {
-                System.out.println(d);
+        double x3, x1, x0;
+        try (Scanner INPUT = new Scanner(System.in)) {
+
+            System.out.println("enter the coefficients: ");
+            x3 = INPUT.nextDouble();
+            x1 = INPUT.nextDouble();
+            x0 = INPUT.nextDouble();
+
+            extended_quard_eq foo = new extended_quard_eq(x3, x1, x0);
+            int retval = foo.has_root();
+
+            if (retval == 1) {
+                System.out.println("root are real & not equal__");
+                double[] x = foo.find_root();
+                for (double d : x) {
+                    System.out.println(d);
+                }
+
+            } else if (retval == 2) {
+                System.out.println("root are imag & not equal__");
+
+            } else {
+                System.out.println("root are real & equal__");
+                double p[] = foo.find_root();
+                for (double d : p) {
+                    System.out.println(d);
+                }
             }
 
-        } else if (retval == 2) {
-            System.out.println("root is imag & not equal__");
-
-        } else {
-            System.out.println("root is real & equal__");
-            double p[] = foo.find_root();
-            for (double d : p) {
-                System.out.println(d);
-            }
+        } catch (Exception e) {
+            System.out.println("____wrong input____ ");
         }
-
     }
 }
+
+/*
+ * 
+ * //when i do....
+ * 
+ * double x3, x1, x0;
+ * try (Scanner INPUT = new Scanner(System.in)) {
+ * System.out.println("enter the coefficients: ");
+ * x3 = INPUT.nextDouble();
+ * x1 = INPUT.nextDouble();
+ * x0 = INPUT.nextDouble();
+ * }
+ * 
+ * //x3 ,x2 ,x0 values are only available in this block....
+ * // so no acessible form here...
+ * 
+ * extended_quard_eq foo = new extended_quard_eq(x3, x1, x0);
+ */
+
+// ? output */
+/*
+ * [🔴] × java com/pkg2/extended_quard_eq
+ * enter the coefficients:
+ * 0
+ * 0
+ * 0
+ * root are real & equal__
+ * NaN
+ * NaN
+ * 
+ * [🔴] × java com/pkg2/extended_quard_eq
+ * enter the coefficients:
+ * 1
+ * 5
+ * 6
+ * root is real & not equal__
+ * -2.0
+ * -3.0
+ */
