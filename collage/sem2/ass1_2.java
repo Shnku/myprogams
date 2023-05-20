7994bc9edf5dcc2affbb713f08f9cdbb0a4a4b0d
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-05-20 11:44:07 +0000

    .

---
+++ collage/sem2/ass1_2.java
@@ -0,0 +1,23 @@
+/*. Write a program to read the price of an item in the decimal form (like 75.95) and print 
+the output in paise (like 7595 paise). */
+
+import java.io.Console;
+import java.text.DecimalFormat;
+
+public class ass1_2 {
+    final static DecimalFormat style = new DecimalFormat("####");
+
+    public static void main(String[] args) throws Exception {
+
+        Console inp = System.console();
+        System.out.println("Enter the number: ");
+        // float data = Float.parseFloat(inp.readLine());
+        String data = inp.readLine();
+        System.out.println("The string before: " + data);
+
+        // String convert = style.format(data);
+        // System.out.println("After converting the no is: " + convert);
+        System.out.println("After converting the no is: " + style.format(data));
+
+    }
+}
