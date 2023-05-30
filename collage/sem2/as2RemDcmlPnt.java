d2ce9740c77d32addc12dbc98854deb4e1894874
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-05-30 21:15:01 +0530

    .

--- collage/sem2/as2RemDcmlPnt.java
+++ collage/sem2/as2RemDcmlPnt.java
@@ -1,4 +1,10 @@
+/* Write a program to read the price of an item in the decimal form,
+(like 75.95) 
+and print the output in paise 
+(like 7595 paise). */
+
 import java.io.Console;
+import java.lang.Math;
 
 public class as2RemDcmlPnt {
 
@@ -7,15 +13,40 @@ public class as2RemDcmlPnt {
 
     static int calculate(Float pointval) {
         as2RemDcmlPnt.pointval = pointval;
-        int count = 0;
-        intval = (int) (pointval * 10);
+
+        String str = String.valueOf(pointval);
+        System.out.println("the string is : " + str);
+        System.out.println("/len=" + str.length() + " \n/point indx: " + str.indexOf('.'));
+
+        int count = str.length() - str.indexOf('.');
+        intval = (int) (pointval * Math.pow(10, count - 1));
         return intval;
     }
 
     /* This is the Main function... */
     public static void main(String[] args) {
 
-        System.out.println("enter a float to convert to int: ");
-        Console input=
+        Console input = System.console();
+
+        if (input != null) {
+            // Float no = Float.parseFloat(input.readLine("En no: "));
+            float no = Float.parseFloat(input.readLine("En te no: "));
+            System.out.println("converted val is: " + calculate(no));
+
+        } else {
+            System.out.println("No terminal availavle...");
+        }
     }
 }
+
+/*
+ * output
+ * 
+ * javac as2RemDcmlPnt.java && java as2RemDcmlPnt
+ * 
+ * En te no: 145.121
+ * the string is : 145.121
+ * /len=7
+ * /point indx: 3
+ * converted val is: 145121
+ */
