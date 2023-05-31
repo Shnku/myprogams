f649e2197493d8c203bee509b103b5d9e216cfc0
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-05-31 15:05:25 +0530

    .

---
+++ collage/sem2/AllNumberChakr.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+/**
+ * Krishnamurthyno. Eg : 145=1! + 4! + 5! .
+ * perfect no. Eg: 6=1+2+3 .
+ */
+
+public class AllNumberChakr {
+
+    static long factorial(int n) {
+        return (n == 0 ? 1 : n * factorial(n - 1));
+    }
+
+    static boolean Krishnamurti_cheaker(int no) {
+        int temp = no;
+        int rem;
+        int sum = 0;
+
+        while (temp != 0) {
+            rem = temp % 10;
+            sum += factorial(rem);
+            temp /= 10;
+        }
+        if (no == sum) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    static boolean perfectno_cheaker(int no) {
+        int temp = no;
+        int rem;
+        int sum = 0;
+
+        while (temp != 0) {
+            rem = temp % 10;
+            sum += rem;
+            temp /= 10;
+        }
+        if (no == sum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter the number: ");
+        int number = input.nextInt();
+        System.out.println("is it krishnamurty no?\t" + Krishnamurti_cheaker(number));
+        System.out.println("is it perfect no?\t" + perfectno_cheaker(number));
+
+        input.close();
+    }
+}
+
+/**
+ ** output
+ * enter the number:
+ * 6
+ * is it krishnamurty no? false
+ * is it perfect no? true
+ * 
+ ** javac AllNumberChakr.java && java AllNumberChakr
+ * enter the number:
+ * 145
+ * is it krishnamurty no? true
+ * is it perfect no? false
+ */
