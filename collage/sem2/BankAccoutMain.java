3bbd28dc53899f7579cb69ca864ffb5e1339e21f
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-06-03 22:23:20 +0530

    .

--- collage/sem2/BankAccoutMain.java
+++ collage/sem2/BankAccoutMain.java
@@ -1,4 +1,6 @@
 import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
 
 class BankAccount {
     String NameOfDepositor;
@@ -6,29 +8,44 @@ class BankAccount {
     String AccountType;
     Double Balance;
 
-    BankAccount(String n, String at) {
-        this.NameOfDepositor = n;
-        this.AccountType = at;
-    }
-
-    BankAccount() {
+    void accountnogenerator() {
+        Random random = new Random();
+        AccountNo = String.valueOf(random.nextInt());
     }
 
     void InitiateBankAccout() {
+        System.out.println("__Welcome to Account creation__");
+        System.out.print("which type of AC you wanna create: ");
+        Scanner sc = new Scanner(System.in);
+        AccountType = sc.nextLine();
+        System.out.println("Enter your name: ");
+        NameOfDepositor = sc.nextLine();
+        sc.close();
+        accountnogenerator();
+        Balance = 0.0;
+    }
 
+    boolean Diposite(double money) {
+        if ((Balance += money) != null)
+            return true;
+        return false;
+    }
+
+    boolean Withdraw(double money) {
+        return (Balance -= money) != null;
     }
 
     void DisplayAC_details() {
         PrintStream sout = new PrintStream(System.out);
-        sout.println("----------------------");
-        sout.printf("| AccountNo: %s\n| Name: %s, \n| AC Type: %s\n", AccountNo, NameOfDepositor, AccountType);
-        sout.println("-----------------------\nCurrent Balance is: " + Balance);
+        sout.println("-----------------------------------");
+        sout.printf("| AccountNo: %-20s |\n| ", AccountNo);
+        sout.printf("Name: %-25s | \n| AC Type: %-22s |\n", NameOfDepositor, AccountType);
+        sout.println("-----------------------------------\nCurrent Balance is: " + Balance);
     }
 }
 
 public class BankAccoutMain {
     public static void main(String[] cmd_line_args) {
-
         BankAccount myAccount = new BankAccount();
         myAccount.InitiateBankAccout();
         myAccount.DisplayAC_details();
