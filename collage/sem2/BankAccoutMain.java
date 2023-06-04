473c8eed8aab5a517f228e5482531effe4037a4a
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-06-04 21:10:22 +0530

    .

--- collage/sem2/BankAccoutMain.java
+++ collage/sem2/BankAccoutMain.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.io.PrintStream;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,7 +8,7 @@ class BankAccount {
     String AccountNo;
     String AccountType;
     Double Balance;
-    String[] options = { "CreateAC", "AC_Details", "Withdraw", "Diposite", "quit" };
+    String[] listOptiion = { "CreateAC", "AC_Details", "Withdraw", "Diposite", "quit" };
 
     void accountnogenerator() {
         Random random = new Random();
@@ -17,35 +18,41 @@ class BankAccount {
     }
 
     void InitiateBankAccout() {
-        System.out.println("__Welcome to Account creation__\n");
-        System.out.print("which type of AC you wanna create: \n");
+        System.out.println("which type of AC you wanna create:--");
         System.out.print("(1) Savings\n(2) Current\n en choice? ");
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()) {
-            int key = sc.nextInt();
-            if (key > 2) {
-                System.out.println("en proper choice");
-            } else {
-                AccountType = key == 1 ? "Savings" : "Current";
-                break;
-            }
-        }
-        sc.nextLine();// cleans previous buffer..
-        System.out.println("Enter your name: ");
-        NameOfDepositor = sc.nextLine();
+        // Scanner sc = new Scanner(System.in);
+        Console input = System.console();
+        // while (sc.hasNextInt()) {
+        // int key = sc.nextInt();
+        // if (key > 2) {
+        // System.out.println("en proper choice");
+        // } else {
+        // AccountType = key == 1 ? "Savings" : "Current";
+        // break;
+        // }
+        // }
+        int key = Integer.parseInt(input.readLine());
+        AccountType = key == 1 ? "Savings" : "Current";
+        // sc.nextLine();// cleans previous buffer..
+        System.out.print("Enter your name: ");
+        NameOfDepositor = input.readLine();
         accountnogenerator();
         Balance = 0.0;
-        sc.close();// free scanner..
+        // sc.close();// free scanner..
     }
 
-    boolean Diposite(double money) {
-        if ((Balance += money) != null)
-            return true;
-        return false;
+    void Diposite() {
+        Console in = System.console();
+        System.out.print("en amount :");
+        double money = Double.parseDouble(in.readLine());
+        Balance += money;
     }
 
-    boolean Withdraw(double money) {
-        return (Balance -= money) != null;
+    void Withdraw() {
+        Console in = System.console();
+        System.out.print("en amount :");
+        double money = Double.parseDouble(in.readLine());
+        Balance -= money;
     }
 
     void DisplayAC_details() {
@@ -56,46 +63,67 @@ class BankAccount {
         sout.println("-----------------------------------\nCurrent Balance is: " + Balance);
     }
 
-    int Choose_option(int choice) {
+    void Choose_option() {
         System.out.println("what do you want to..");
-        for (String s : options) {
-            System.out.println(s);
+        for (int i = 0; i < listOptiion.length; i++) {
+            System.out.printf("(%d) %s\n", i + 1, listOptiion[i]);
         }
         System.out.println("en choice: ");
-        Scanner sc = new Scanner(System.in);
-
-        while (options.equals("quit")) {
+        int choice;
+        try (Scanner coin = new Scanner(System.in)) {
+            choice = coin.nextInt();
+        }
+        while (true) {
             switch (choice) {
                 case 1:
                     InitiateBankAccout();
                     System.out.println("account created");
-                    break;
+                    continue;
                 case 2:
                     DisplayAC_details();
-                    break;
+                    continue;
                 case 3:
-                    double money = sc.nextDouble();
-                    Withdraw(money);
-                    break;
+                    Withdraw();
+                    continue;
                 case 4:
-                    double mony = sc.nextDouble();
-                    Diposite(mony);
-                    break;
+                    Diposite();
+                    continue;
                 case 5:
-
+                    System.out.println("exit");
+                    System.exit(0);
                 default:
                     System.out.println("enter proper choice");
-                    break;
+                    continue;
             }
         }
-        sc.close();
-        return choice;
     }
 }
 
 public class BankAccoutMain {
     public static void main(String[] cmd_line_args) {
         BankAccount myAccount = new BankAccount();
+        myAccount.Choose_option();
 
     }
 }
+
+// !error...
+
+/*
+ * what do you want to..
+ * (1) CreateAC
+ * (2) AC_Details
+ * (3) Withdraw
+ * (4) Diposite
+ * (5) quit
+ * en choice:
+ * 1
+ * which type of AC you wanna create:--
+ * (1) Savings
+ * (2) Current
+ * en choice? Exception in thread "main" java.lang.NullPointerException: Cannot
+ * invoke "java.io.Console.readLine()" because "<local1>" is null
+ * at BankAccount.InitiateBankAccout(BankAccoutMain.java:34)
+ * at BankAccount.Choose_option(BankAccoutMain.java:79)
+ * at BankAccoutMain.main(BankAccoutMain.java:105)
+ */
