676f55fc9fb44e36dedf1c555987b1c3fec971d3
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-06-07 19:00:24 +0530

    .

---
+++ collage/sem2/StringNameform.java
@@ -0,0 +1,26 @@
+/*Take a full name as input and print the intitials . 
+If Input is “Mohandas Karamchand Gandhi” 
+output – “M.K.Gandhi” */
+
+public class StringNameform {
+
+    static String method(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = s.length() - 1; s.charAt(i) != ' '; i--) {
+            result.append(s.charAt(i));
+        }
+        result.reverse();
+        // return (String.valueOf(result));
+        return result.toString();
+    }
+
+    static void method() {
+
+    }
+
+    public static void main(String[] args) {
+        String str = new String("this is a string");
+        str = method(str);
+        System.out.println(str);
+    }
+}
--- collage/sem2/input.java
+++ collage/sem2/input.java
@@ -1,40 +1,60 @@
 import java.io.DataInputStream;
 import java.io.IOException;
 
-// class input {
-//     public static void main(String[] args) throws IOException {
-//         DataInputStream get = new DataInputStream(System.in);
-//         System.out.println("enter a integer: ");
+class input {
+    public static void main(String[] args) throws IOException {
+        DataInputStream get = new DataInputStream(System.in);
+        System.out.println("enter a sentense: ");
 
-//         //String s = String.valueOf(get.readLine());
-//         String s = get.readLine(); 
-//        
-//         System.out.println("you enterd: " + s);
-//     }
-// }
+        // String s = String.valueOf(get.readLine());
+        String s = get.readLine();
 
-//! readLine() method from the DataInputStream class is deprecated.
+        System.out.println("enter a integer: ");
+        int i = Integer.parseInt(get.readLine());
+        System.out.println("you enterd: " + s + " " + i);
+    }
+}
+
+/*
+ * Note: input.java uses or overrides a deprecated API.
+ * Note: Recompile with -Xlint:deprecation for details.
+ * enter a sentense:
+ * this
+ * enter a integer:
+ * 5
+ * you enterd: this
+ */
+/*
+ * javac -Xlint input.java
+ * input.java:10: warning: [deprecation] readLine() in DataInputStream has been
+ * deprecated
+ * String s = get.readLine();
+ * ^
+ * 1 warning
+ */
+
+// ! readLine() method from the DataInputStream class is deprecated.
 
 /**
  * input
  */
 
-public class input {
-    public static void main(String[] args) throws IOException {
-        DataInputStream input = new DataInputStream(System.in);
+// public class input {
+// public static void main(String[] args) throws IOException {
+// DataInputStream input = new DataInputStream(System.in);
 
-        System.out.print("Enter an integer: ");
-        int number = input.readInt();
-        System.out.println("You entered: " + number);
+// System.out.print("Enter an integer: ");
+// int number = input.readInt();
+// System.out.println("You entered: " + number);
 
-        System.out.print("Enter a floating-point number: ");
-        float floatNumber = input.readFloat();
-        System.out.println("You entered: " + floatNumber);
+// System.out.print("Enter a floating-point number: ");
+// float floatNumber = input.readFloat();
+// System.out.println("You entered: " + floatNumber);
 
-        // Close the DataInputStream
-        input.close();
-    }
-}
+// // Close the DataInputStream
+// input.close();
+// }
+// }
 
 /*
  * Enter an integer: 456
