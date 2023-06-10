c7ddb9f86f4128269a941f2d2cc663699a16cee3
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-06-10 10:34:11 +0530

    .

---
+++ collage/sem2/AlplaOrderedString.java
@@ -0,0 +1,12 @@
+/*Write a program in Java which will read a string and rewrite it in the alphabetical order. 
+For example, the word STRING should be written as GINRST */
+
+public class AlplaOrderedString {
+    public static void alphaOrder(String string) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
