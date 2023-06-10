a1a9b1a1e5402588c068ca2156c30f06171574fd
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-06-10 11:59:28 +0530

    .

--- collage/sem2/AlplaOrderedString.java
+++ collage/sem2/AlplaOrderedString.java
@@ -3,10 +3,21 @@ For example, the word STRING should be written as GINRST */
 
 public class AlplaOrderedString {
     public static void alphaOrder(String string) {
-
+        char str_arr[] = string.toCharArray();
+        for (int i = 0; i < string.length() - 1; i++) {
+            for (int j = 0; j < string.length() - i - 1; j++) {
+                if (string.charAt(j) > string.charAt(j + 1)) {
+                    char temp = str_arr[i];
+                    str_arr[i] = str_arr[j];
+                    str_arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(str_arr);
     }
 
     public static void main(String[] args) {
-
+        String str = "STRING";
+        alphaOrder(str);
     }
 }
