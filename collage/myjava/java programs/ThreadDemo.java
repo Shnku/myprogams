91d562396d772a4330f42d235c8c9822b24ef5f2
shanku <shankubag04@gmail.com>
2023-06-27 17:10:37 +0530

    .

---
+++ collage/myjava/java programs/ThreadDemo.java
@@ -0,0 +1,46 @@
+public class ThreadDemo implements Runnable {
+    Thread t;
+    String n;
+
+    ThreadDemo(String name) {
+        System.out.println("creating " + name + "thread.");
+        this.n = name;
+        t = new Thread(n);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(n + " Thread running.... start.");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(" " + (i + 1) + "th ...");
+        }
+        System.out.println("exiting thread..");
+    }
+
+    public static void main(String[] args) {
+        ThreadDemo d1 = new ThreadDemo("main");
+        ThreadDemo d2 = new ThreadDemo("chield");
+        d1.run();
+        d2.run();
+    }
+}
+
+// putput..
+/*
+ * creating mainthread.
+ * creating chieldthread.
+ * main Thread running.... start.
+ * 1th ...
+ * 2th ...
+ * 3th ...
+ * 4th ...
+ * 5th ...
+ * exiting thread..
+ * chield Thread running.... start.
+ * 1th ...
+ * 2th ...
+ * 3th ...
+ * 4th ...
+ * 5th ...
+ * exiting thread..
+ */
