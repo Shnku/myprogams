8241df8b54e7c39b8774e5d6dd7cea9bae456280
shanku <shankubag04@gmail.com>
2023-07-06 17:02:18 +0530

    .

--- collage/myjava/java programs/IffitInType.java
+++ collage/myjava/java programs/IffitInType.java
@@ -22,11 +22,11 @@ public class IffitInType {
                 System.out.println("*double");
                 canbeparsed = true;
 
-                long l = Long.parseLong(s[i]);
-                System.out.println("*long");
-
                 float f = Float.parseFloat(s[i]);
                 System.out.println("*float");
+
+                long l = Long.parseLong(s[i]);
+                System.out.println("*long");
 
                 int d = Integer.parseInt(s[i]);
                 System.out.println("*int");
@@ -49,3 +49,42 @@ public class IffitInType {
         sc.close();
     }
 }
+
+// output
+/*
+ * enter the input number:
+ * 5
+ * en data 1:
+ * -15.2656
+ * en data 2:
+ * -153
+ * en data 3:
+ * 4.32
+ * en data 4:
+ * 56798798798.5789798
+ * en data 5:
+ * 4787897897987789798787878799
+ * 
+ * -15.2656 can be fitted in
+ * double
+ * float
+ * 
+ * -153 can be fitted in
+ * double
+ * float
+ * long
+ * int
+ * short
+ * 
+ * 4.32 can be fitted in
+ * double
+ * float
+ * 
+ * 56798798798.5789798 can be fitted in
+ * double
+ * float
+ * 
+ * 4787897897987789798787878799 can be fitted in
+ * double
+ * float
+ */
