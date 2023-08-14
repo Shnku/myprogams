ab9821f7757dadbe3732d9d7744ee1997eb45f25
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-08-14 15:58:41 +0530

    .

--- collage/dsa/linklistcreat.c
+++ collage/dsa/linklistcreat.c
@@ -4,18 +4,29 @@
 typedef struct list
 {
     int list_data;
-    int link_to_nxts_addr;
+    struct list *link_to_nxts_addr;
 } LIST;
 
-void add_to_list(LIST **head_ptr, int data)
+LIST *head_pointer = NULL;
+LIST *current_node = NULL;
+
+void add_to_list(int data)
 {
-    LIST *node = NULL;
+    LIST *iterator = head_pointer;
+    LIST *node = (LIST *)malloc(sizeof(LIST));
     node->list_data = data;
+    node->link_to_nxts_addr = NULL;
+    while (head_pointer->link_to_nxts_addr != NULL)
+    {
+        iterator = iterator->link_to_nxts_addr;
+    }
+    current_node = iterator;
+    current_node->link_to_nxts_addr = node;
 }
 
 int main()
 {
-    int *head_pointer = NULL;
+
     printf("en the data: ");
 
     return 0;
