f65ce4c10ab4595aca4fb805fc7b56c46867cf6b
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-09-03 16:23:36 +0530

    .

---
+++ collage/myjava/snwyam_nptel/ThreadClassMain1.java
@@ -0,0 +1,87 @@
+class ThreadA extends Thread {
+    public void run() { // void run() //error..
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("from thread A with: " + (i * -1));
+        }
+        System.out.println("__Exiting from thread A");
+    }
+}
+
+class ThreadB implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("from thread B with: " + (i * 10));
+        }
+        System.out.println("__Exiting from thread B");
+    }
+
+    public void start() {
+        run();
+    }
+}
+
+class ThreadC extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("from thread C with: " + (i * -100));
+        }
+        System.out.println("__Exiting from thread C");
+    }
+}
+
+public class ThreadClassMain1 {
+    public static void main(String[] args) {
+        ThreadA tA = new ThreadA();
+        ThreadB tB = new ThreadB();
+        ThreadC tC = new ThreadC();
+        tA.start();
+        tB.start();
+        tC.start();
+
+    }
+}
+
+/*result 1
+
+from thread A with: -1
+from thread A with: -2
+from thread A with: -3
+from thread A with: -4
+from thread A with: -5
+from thread B with: 10
+__Exiting from thread A
+from thread B with: 20
+from thread B with: 30
+from thread B with: 40
+from thread B with: 50
+__Exiting from thread B
+from thread C with: -100
+from thread C with: -200
+from thread C with: -300
+from thread C with: -400
+from thread C with: -500
+__Exiting from thread C
+*/
+
+/* result 2
+
+from thread B with: 10
+from thread A with: -1
+from thread B with: 20
+from thread A with: -2
+from thread B with: 30
+from thread A with: -3
+from thread B with: 40
+from thread B with: 50
+from thread A with: -4
+__Exiting from thread B
+from thread A with: -5
+__Exiting from thread A
+from thread C with: -100
+from thread C with: -200
+from thread C with: -300
+from thread C with: -400
+from thread C with: -500
+__Exiting from thread C
+ 
+ */
