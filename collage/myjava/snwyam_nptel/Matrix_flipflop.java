8ff91860a5fa991442bbd9d959220cde3f6e411a
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-09-26 22:30:06 +0530

    calculator

---
+++ collage/myjava/snwyam_nptel/Matrix_flipflop.java
@@ -0,0 +1,30 @@
+/*
+Write suitable code to develop a 2D Flip-Flop Array with dimension 5 × 5, which replaces all input elements with values 0 by 1 and 1 by 0. An example is shown below:
+
+INPUT:
+               00001
+               00001
+               00001
+               00001
+               00001
+
+OUTPUT:
+
+               11110
+               11110
+               11110
+               11110
+               11110
+
+Note the following points carefully:
+1. Here, the input must contain only 0 and 1.
+2. The input and output array size must be of dimension 5 × 5.
+3. Flip-Flop: If 0 then 1 and vice-versa.
+
+*/
+
+public class Matrix_flipflop {
+    public static void main(String[] args) {
+
+    }
+}
