6dd6102bb9e4e8a6a674e0f4bb3ddb63a322cbad
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-09-26 22:18:32 +0530

    calculator

---
+++ collage/myjava/snwyam_nptel/Matrix_reflect.java
@@ -0,0 +1,23 @@
+/*
+ class Matrix_reflect
+ Problem statement:
+
+A program needs to be developed which can mirror reflect any 5 × 5 2D character array into its side-by-side reflection. Write suitable code to achieve this transformation as shown below:
+
+ INPUT:                                       OUTPUT:
+               OOXOO                                        OOXOO
+               OOXOO                                        OOXOO
+               XXXOO                                        OOXXX
+               OOOOO                                        OOOOO
+               XOABC                                        CBAOX
+
+Note the following points carefully:
+1. Here, instead of X and O any character may be present.
+2. The input and output array size must be of dimension 5 × 5 and nothing else.
+3. Only side-by-side reflection should be performed i.e. ABC || CBA.
+
+*/
+
+public class Matrix_reflect {
+
+}
---
+++ collage/myjava/snwyam_nptel/Matrix_rotate.java
@@ -0,0 +1,23 @@
+/*
+Complete the code to perform a 45 degree anti clock wise rotation with respect to the center of a 5 × 5 2D Array as shown below:
+
+INPUT:
+              00100
+              00100
+              11111
+              00100
+              00100
+OUTPUT:
+              10001
+              01010
+              00100
+              01010
+              10001
+
+ */
+
+public class Matrix_rotate {
+    public static void main(String[] args) {
+        
+    }
+}
