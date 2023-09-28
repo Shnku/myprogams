730b007bd6dd749bb617b7aae8cc228ccbc17cc2
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-09-28 21:35:16 +0530

    matrix fliplpo done

--- collage/myjava/snwyam_nptel/gui_s/Matrix_flipflop.java
+++ collage/myjava/snwyam_nptel/gui_s/Matrix_flipflop.java
@@ -23,8 +23,64 @@ Note the following points carefully:
 
 */
 
+import java.util.Scanner;
+
 public class Matrix_flipflop {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int mat[][] = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                short chance = 4;
+                while (chance > 0) {
+                    try {
+                        int input = sc.nextInt();
+                        if (input < 0 || input > 1)
+                            throw new Exception();
+
+                        mat[i][j] = input;
+                        break;
+
+                    } catch (Exception e) {
+                        System.out.println("enter 1 or 0 only");
+                        chance--;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == 0) {
+                    mat[i][j] = 1;
+                } else
+                    mat[i][j] = 0;
+            }
+        }
+
+        for (int[] is : mat) {
+            for (int is2 : is) {
+                System.out.print(is2);
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
+
+//output
+/*
+1 0 0 0 0
+0 1 0 1 0
+0 1 0 1 0
+0 0 0 0 0
+0 0 0 0 0
+
+01111
+10101
+10101
+11111
+11111
+ */
