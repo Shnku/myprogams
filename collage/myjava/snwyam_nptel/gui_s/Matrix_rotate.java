6e14f8b1d131c2a4e776b971166b42dc45a1a72d
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-09-30 11:42:14 +0530

    matrix rotate not done prpperly

--- collage/myjava/snwyam_nptel/gui_s/Matrix_rotate.java
+++ collage/myjava/snwyam_nptel/gui_s/Matrix_rotate.java
@@ -18,6 +18,94 @@ OUTPUT:
 
 public class Matrix_rotate {
     public static void main(String[] args) {
-        
+        int row = 5, col = 5;
+        // int mat1[][]=new int[row][col];
+
+        int[][] mat1 = {
+                { 8, 8, 1, 8, 8 }, // 00 01 02 03 04
+                { 8, 8, 1, 8, 8 }, // 10 11 12 13 14
+                { 1, 1, 1, 1, 1 }, // 20 21 22 23 24
+                { 8, 8, 1, 8, 8 }, // 30 31 32 33 34
+                { 8, 8, 1, 8, 8 } /// 40 41 42 43 44
+        };
+
+        int mat2[][] = new int[row][col];
+        int mid = row / 2;
+
+        //upper half...
+        int pos = mid;
+        for (int i = 0; i < mid; i++) {
+            for (int j = mid; j < col; j++) {
+                mat2[i][j - pos] = mat1[i][j];
+            }
+            pos--;
+        }
+        display(mat2);
+        //lower half...
+        pos = mid;
+        for (int i = row - 1; i > mid; i--) {
+            for (int j = mid; j >= 0; j--) {
+                mat2[i][j + pos] = mat1[i][j];
+            }
+            pos--;
+        }
+        display(mat2);
+
+        //rotate/...
+        //upper half...
+        pos = mid;
+        for (int j = col - 1; j > mid; j--) {
+            for (int i = mid; i < row; i++) {
+                mat2[i - pos][j] = mat1[i][j];
+            }
+            pos--;
+        }
+        display(mat2);
+        //lower half...
+        pos = mid;
+        for (int j = 0; j < mid; j++) {
+            for (int i = mid; i >= 0; i--) {
+                mat2[i + pos][j] = mat1[i][j];
+            }
+            pos--;
+        }
+        display(mat2);
+    }
+
+    static void display(int mat2[][]) {
+        System.out.println("display...");
+        for (int[] is : mat2) {
+            for (int is2 : is) {
+                System.out.printf("%3d ", is2);
+            }
+            System.out.println();
+        }
     }
 }
+
+/*
+display...
+  1   8   8   0   0 
+  0   1   8   8   0 
+  0   0   0   0   0 
+  0   0   0   0   0 
+  0   0   0   0   0 
+display...
+  1   8   8   0   0 
+  0   1   8   8   0 
+  0   0   0   0   0 
+  0   8   8   1   0 
+  0   0   8   8   1 
+display...
+  1   8   8   0   1 
+  0   1   8   1   8 
+  0   0   0   8   8 
+  0   8   8   8   0 
+  0   0   8   8   1 
+display...
+  1   8   8   0   1 
+  0   8   8   1   8 
+  8   8   0   8   8 
+  8   1   8   8   0 
+  1   0   8   8   1 
+ */
