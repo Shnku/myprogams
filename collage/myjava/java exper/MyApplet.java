9c64855233b33a9acf6f727a8c142079862e5c91
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-10-15 00:28:32 +0530

    applet works

--- collage/myjava/java exper/MyApplet.java
+++ collage/myjava/java exper/MyApplet.java
@@ -1,8 +1,14 @@
 import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class MyApplet extends Applet {
+    public void init() {
+        setBackground(Color.GREEN);
+    }
+
     public void paint(Graphics g) {
-        g.drawString("hellow applet", 150, 150);
+        g.drawString("this is an applet window", 150, 25);
+        showStatus("this is shown status window..");
     }
 }
