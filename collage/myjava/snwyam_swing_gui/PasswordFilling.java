f3757a992006967a33d324d3d0598a359269d016
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-10-25 17:02:07 +0530

    panel password checkbox done

--- collage/myjava/snwyam_swing_gui/PasswordFilling.java
+++ collage/myjava/snwyam_swing_gui/PasswordFilling.java
@@ -3,15 +3,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class PasswordFilling implements ActionListener {
     JFrame f = new JFrame("demo password field");
-    JPanel p;
+    JPanel p1JPanel, p2;
     JButton b;
     JLabel input1, input2;
     final JTextField uname;
@@ -29,8 +32,12 @@ public class PasswordFilling implements ActionListener {
     }
 
     PasswordFilling() {
-        p = new JPanel();
-        p.setBounds(20, 20, 200, 200);
+        p1JPanel = new JPanel();
+        p1JPanel.setBounds(10, 20, 200, 200);
+        p1JPanel.setBackground(Color.gray);
+        p2 = new JPanel();
+        p2.setBounds(250, 20, 160, 200);
+        p2.setBackground(Color.darkGray);
 
         input1 = new JLabel("username: ");
         uname = new JTextField(10);
@@ -42,15 +49,23 @@ public class PasswordFilling implements ActionListener {
 
         l = new JLabel();
 
-        p.add(input1);
-        p.add(uname);
-        p.add(input2);
-        p.add(pas);
-        p.add(b);
-        p.add(l);
-        p.setBackground(Color.gray);
+        p1JPanel.add(input1);
+        p1JPanel.add(uname);
+        p1JPanel.add(input2);
+        p1JPanel.add(pas);
+        p1JPanel.add(b);
+        p1JPanel.add(l);
 
-        f.add(p);
+        JCheckBox c1 = new JCheckBox("checknox");
+        JRadioButton r1 = new JRadioButton("JRadioButton");
+        JCheckBoxMenuItem cbm1 = new JCheckBoxMenuItem("kBoxMenuItem");
+
+        p2.add(c1);
+        p2.add(r1);
+        p2.add(cbm1);
+
+        f.add(p1JPanel);
+        f.add(p2);
         f.setLayout(null);
         f.setSize(500, 400);
         f.setVisible(true);
