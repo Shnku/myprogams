e5630fd0ba0a283f8fb1e6128cade40701a633d2
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-12-12 09:27:05 +0530

    add delete before after full done.... but not running test..

--- collage/dsa/sing_add_before_&_after.h
+++ collage/dsa/sing_add_before_&_after.h
@@ -58,4 +58,44 @@ LIST *add_before(LIST *head, int before, int data)
 void add_aft(LIST **head, int after, int data)
 {
     LIST *temp, *ptr;
+    if (!(*head))
+    {
+        printf("list is empty....\n");
+        return;
+    }
+    ptr = *head;
+    while (ptr != NULL && ptr->data != after)
+        ptr = ptr->nxt_p;
+    if (ptr == NULL)
+        printf("\nlement not found");
+    else
+    {
+        temp = (LIST *)malloc(sizeof(LIST));
+        temp->data = data;
+        temp->nxt_p = ptr->nxt_p;
+        ptr->nxt_p = temp;
+    }
+}
+
+LIST *add_after(LIST *head, int after, int data)
+{
+    LIST *temp, *ptr = head;
+    if (!head)
+    {
+        printf("list is empty...\n");
+        return;
+    }
+    while (ptr != NULL)
+    {
+        if (ptr->data == after)
+        {
+            temp = (LIST *)malloc(sizeof(LIST));
+            temp->data = data;
+            temp->nxt_p = ptr->nxt_p;
+            ptr->nxt_p = temp;
+            return head;
+        }
+        ptr = ptr->nxt_p;
+    }
+    printf("%d not present in list..", after);
 }
