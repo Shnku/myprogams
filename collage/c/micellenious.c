5f6dac9f12052a69add3b942a9bec5ed0c306574
shnku <sbag04@gmail.com>
2024-02-13 10:59:22 +0000

    misclenious c

--- collage/c/micellenious.c
+++ collage/c/micellenious.c
@@ -72,3 +72,41 @@ int main()
     return 0;
 }
 #endif
+
+/*
+________what scanf_printf returns______
+*/
+#if 0
+int main() {
+  int a = printf("\nthis is qq 33111");
+  int b = printf("\n%d", a);
+  int c = printf("\n%d\n\t", b);
+  printf("%d\n", c);
+
+//   int d = scanf("%d", &a);
+//   printf("%d\n\n", d);
+
+  char f[100];
+  int e = scanf("%s", f);
+  printf("%d\n", e);
+}
+/*17
+3
+        4
+thisis
+1
+*/
+#endif
+
+
+#if 0
+int main() {
+  char c[100];
+  scanf("%[A-Z a-z ]", c);
+  printf("%s\n", c);
+}
+/*
+this is the my thibg!! @#$
+this is the my thibg
+*/
+#endif
